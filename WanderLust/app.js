7508a67425ff49b02f2717d3abd141416231797e
Shaikh Tauhid Shaikh Javed <tauhidshaikhsas@gmail.com>
2025-08-27 23:56:47 +0530

    made a lot of changes

--- WanderLust/app.js
+++ WanderLust/app.js
@@ -3,25 +3,32 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing")
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
 })
-app.get("/",(req,res)=>{
-    res.send("Hi I am root")
-})
-main().catch(err => console.log(err));
+app.use(express.static(path.join(__dirname,'public')));
 
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust'
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+app.get("/",(req,res)=>{
+    res.send("Hi I am root")
+})
+main().catch(err => console.log(err));
 main().then(()=>{
     console.log("DATA BASE CONNECT HO GAYA BHAI!!")
 }).catch((err)=>{
-    console.log("ISKIMAA KA KUCH :",err);
+    console.log("ISKIMAA KA  :",err);
 })
 // app.get('/testlisting',async(req,res)=>{
 //     let smaplelisint = new Listing({
@@ -44,6 +51,33 @@ app.get("/listings", async (req,res)=>{
     res.render("listing/index.ejs",{allListing})
 })
 
+app.get("/listings/new",(req,res)=>{
+    res.render("listing/new.ejs");
+})
+
+app.post("/listings" ,async(req,res)=>{
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect(('/listings'))
+})
+//edit route
+app.get('/listings/:id/edit', async(req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listing/edit.ejs",{listing});
+})
+//update route
+app.put('/listings/:id',async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`)
+})
+app.delete("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    let deleted = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+    console.log(deleted);
+})
 app.get("/listings/:id", async(req,res)=>{
     const {id} = req.params;
     let listing = await Listing.findById(id);
